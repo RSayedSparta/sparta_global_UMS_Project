794c13f6c5468d8dddbd6435fd1ece42cae1a7db
rahib <rsayed@spartaglobal.com>
2019-03-26 11:08:07 +0000

    Altered the tables on db

--- UMS_Project/UMS_Project/UMModel.Context.cs
+++ UMS_Project/UMS_Project/UMModel.Context.cs
@@ -29,5 +29,6 @@ namespace UMS_Project
         public virtual DbSet<Role> Roles { get; set; }
         public virtual DbSet<Stream> Streams { get; set; }
         public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Trainer> Trainers { get; set; }
     }
 }
