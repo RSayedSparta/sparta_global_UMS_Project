6237a98ce85bbded1b524737c25b28e52bc6d499
yolanda <ysimpson@spartaglobal.com>
2019-03-27 11:45:45 +0000

    work added

--- UMS_Project/UMS_Project/User.cs
+++ UMS_Project/UMS_Project/User.cs
@@ -36,6 +36,7 @@ namespace UMS_Project
         [RegularExpression(@"^[a-zA-Z0-9._%+-]+(@spartaglobal\.com)$", ErrorMessage = "Registration limited to 'spartaglobal.com'.")]
         [StringLength(30, MinimumLength = 8, ErrorMessage = "Invalid")]
         public string email { get; set; }
+        //[R]
         [Required]
         [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[a-z])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "Must contain at least one lowercase letter, one uppercase letter and one number")]
         public string upassword { get; set; }
