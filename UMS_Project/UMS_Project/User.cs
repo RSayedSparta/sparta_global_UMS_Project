93937f6637b26b5d574fe447d7ac9743215fd392
Faduma Hussein <fhussein@spartglobal.com>
2019-03-27 11:39:41 +0000

    nav bar done

--- UMS_Project/UMS_Project/User.cs
+++ UMS_Project/UMS_Project/User.cs
@@ -11,6 +11,7 @@ namespace UMS_Project
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
 
