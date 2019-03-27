dd3afbfc3bd278c15c7e0147e208fc0c2f9cc11f
rahib <rsayed@spartaglobal.com>
2019-03-27 15:57:12 +0000

    Authentication for User and Admin is working

--- UMS_Project/UMS_Project/UMModel.Designer.cs
+++ UMS_Project/UMS_Project/UMModel.Designer.cs
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 // T4 code generation is enabled for model 'C:\Users\Tech-W96a\Engineering26\week11\sparta_global_UMS_Project\UMS_Project\UMS_Project\UMModel.edmx'. 
 
 ﻿// T4 code generation is enabled for model 'C:\Users\tech-w99a.LAPTOP-3BLG1NHP\Engineering26\week11\sparta_global_UMS_Project\UMS_Project\UMS_Project\UMModel.edmx'. 
@@ -17,9 +16,9 @@
 
 
 ﻿// T4 code generation is enabled for model 'C:\Users\tech-w94a\Engineering26\Week11\sparta_global_UMS_Project\UMS_Project\UMS_Project\UMModel.edmx'.
-=======
+
 // T4 code generation is enabled for model 'C:\Users\tech-w94a\Engineering26\Week11\sparta_global_UMS_Project\UMS_Project\UMS_Project\UMModel.edmx'. 
->>>>>>> 22bdccba2b4894fc81554ef4a18a7f865f69512e
+
 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
