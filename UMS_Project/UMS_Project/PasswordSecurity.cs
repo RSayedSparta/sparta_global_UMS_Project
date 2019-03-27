483c5a0d688c499f6263cfdc85cfd9923e836d61
Mishu <mjahur@spartaglobal.com>
2019-03-27 11:44:36 +0000

    begun password salt and hash

---
+++ UMS_Project/UMS_Project/PasswordSecurity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace UMS_Project
+{
+    public class PasswordSecurity
+    {
+        public static string GenerateSalt(int length)
+        {
+            var bytes = new byte[length];
+
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(bytes);
+            }
+
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static byte[] GenerateHash(byte[] password, byte[] salt, int iterations, int length)
+        {
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, salt, iterations))
+            {
+                return deriveBytes.GetBytes(length);
+            }
+        }
+    }
+}
--- UMS_Project/UMS_Project/User.cs
+++ UMS_Project/UMS_Project/User.cs
@@ -36,15 +36,20 @@ namespace UMS_Project
         [RegularExpression(@"^[a-zA-Z0-9._%+-]+(@spartaglobal\.com)$", ErrorMessage = "Registration limited to 'spartaglobal.com'.")]
         [StringLength(30, MinimumLength = 8, ErrorMessage = "Invalid")]
         public string email { get; set; }
-        [Required]
-        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[a-z])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "Must contain at least one lowercase letter, one uppercase letter and one number")]
-        public string upassword { get; set; }
+        //[Required]
+        //[RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[a-z])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "Must contain at least one lowercase letter, one uppercase letter, one number and have at least characters")]
+        //public string upassword { get; set; }
         public string passwordSalt { get; set; }
         public string passwordHash { get; set; }
         [Required]
         public int roleID { get; set; }
         //[Required]
         public int cohortID { get; set; }
+
+        [NotMapped] 
+        [Required]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[a-z])[A-Za-z\d@$!%*#?&]{8,}$", ErrorMessage = "Must contain at least one lowercase letter, one uppercase letter and one number")]
+        public string password { get; set; }
 
         //[NotMapped] 
         //[Required]
