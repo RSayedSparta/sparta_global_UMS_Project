17388c2ff8e64304ed6352b098f4ce09d46667a0
Rume Rhuada <rumerhuada@spartaglobal.com>
2019-04-08 10:10:50 +0100

    fixed issue

--- UMS_Project/UMS_Project/Cohort.cs
+++ UMS_Project/UMS_Project/Cohort.cs
@@ -11,9 +11,7 @@ namespace UMS_Project
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Cohort
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -22,25 +20,15 @@ namespace UMS_Project
             this.Trainers = new HashSet<Trainer>();
             this.Users = new HashSet<User>();
         }
-        [Required]
+    
         public int cohortID { get; set; }
-        [Required]
-        [DisplayName("Cohort Name")]
         public string cohortName { get; set; }
-        [DisplayName("Start Date")]
         public Nullable<System.DateTime> startDate { get; set; }
-        [DisplayName("End Date")]
         public Nullable<System.DateTime> endDate { get; set; }
-        [DisplayName("Has TA")]
         public Nullable<bool> hasTA { get; set; }
-        [Required]
-        [DisplayName("Location")]
         public string clocation { get; set; }
-        [DisplayName("Max Capacity")]
         public Nullable<int> maximumSeats { get; set; }
-        [DisplayName("Min capacity")]
         public Nullable<int> minimumSeats { get; set; }
-        [Required]
         public int streamID { get; set; }
     
         public virtual Stream Stream { get; set; }
--- UMS_Project/UMS_Project/DeletedUser.cs
+++ UMS_Project/UMS_Project/DeletedUser.cs
@@ -11,17 +11,11 @@ namespace UMS_Project
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations.Schema;
-
-    public partial class User
-    {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public User()
-        {
-            this.Trainers = new HashSet<Trainer>();
-        }
     
-        public int userID { get; set; }
+    public partial class DeletedUser
+    {
+        public int id { get; set; }
+        public Nullable<int> userID { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public Nullable<int> age { get; set; }
@@ -29,13 +23,7 @@ namespace UMS_Project
         public string email { get; set; }
         public string passwordSalt { get; set; }
         public string passwordHash { get; set; }
-        public int roleID { get; set; }
-        public int cohortID { get; set; }
-        [NotMapped]
-        public string password { get; set; }
-        public virtual Cohort Cohort { get; set; }
-        public virtual Role Role { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Trainer> Trainers { get; set; }
+        public Nullable<int> roleID { get; set; }
+        public Nullable<int> cohortID { get; set; }
     }
 }
