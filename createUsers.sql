4fff741a95ed0e856b982e045bb4541eb749ca82
Matthew Shaw <mshaw@sparta.com>
2019-04-09 09:29:11 +0100

    pre merge

--- createUsers.sql
+++ createUsers.sql
@@ -1,5 +1,22 @@
 INSERT INTO Users VALUES
- ('Markson', 'M', 20, 'Male', 'Markson@spartaglobal.com','Sparta123', 'a', 'a', '1', '1'),
- ('Jo', 'H', 20, 'Female', 'Jo@spartaglobal.com', 'Sparta123', 'a', 'a', '1', '1'),
- ('Zoe', 'B', 20, 'Female', 'Zoe@spartaglobal.com','Sparta123', 'a', 'a', '1', '2')
-
+ ('Markson', 'M', 20, 'Male', 'Markson@spartaglobal.com', NULL, NULL, '1', '1'),
+ ('Jo', 'H', 20, 'Female', 'Jo@spartaglobal.com', NULL, NULL, '1', '1'),
+ ('Zoe', 'B', 20, 'Female', 'Zoe@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Nikola', 'Tesla', 65, 'Male', 'NTesla@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Albert', 'Einstein', 42, 'Male', 'AEinstein@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Stephen', 'Hawkin', 17, 'Male', 'SHawkins@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Alan', 'Turing', 30, 'Male', 'ATuring@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Ada', 'Lovelace', 28, 'Female', 'ALovelace@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Tim', 'Bernard-Lee', 48, 'Male', 'TBernard@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Linus', 'Torvalds', 65, 'Male', 'LTorvalds@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Vincent', 'Gogh', 37, 'Male', 'VGogh@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Leonardo', 'Vinci', 80, 'Male', 'LVinci@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Pablo', 'Picasso', 54, 'Male', 'PPicasso@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Barack', 'Obama', 45, 'Male', 'BObama@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Donald', 'Trump', 48, 'Male', 'DTrump@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('David', 'Cameron', 52, 'Male', 'DCameroon@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Tony', 'Blair', 43, 'Male', 'TBlair@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Margaret', 'Hamilton', 43, 'Female', 'MHamilton@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Theresa', 'May', 68, 'Female', 'TMayr@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Angela', 'Merkle', 43, 'Male', 'TBlair@spartaglobal.com', NULL, NULL, '1', '2'),
+ ('Florence', 'Nightingale', 43, 'Male', 'TBlair@spartaglobal.com', NULL, NULL, '1', '2')
---
+++ sql/Tables/CreateCohorts.sql
@@ -0,0 +1,4 @@
+INSERT INTO Cohorts VALUES
+ ('Engineering 26', '2019/08/01', '2019/04/01', 'False', 'Richmond','5', '20', '1'),
+ ('Engineering 24', '2018/12/15', '2019/03/21', 'True', 'Richmond','5', '20', '1'),
+ ('Business 18', '2019/02/01', '4/3/2019', 'True', 'Bristol','2', '15', '2');
